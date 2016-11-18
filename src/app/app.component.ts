6f807d7e1ac036ff0c06648f0a59bf8def612527
Magnus Grindal Bakken <magnusbakken@gmail.com>
2016-11-18 13:21:26 +0100

    removed stuff from app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { Article } from './article';
-import { ArticleService } from './article.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  articles: Article[];
-
-  constructor(
-    private articleService: ArticleService
-  ) {
-    articleService.getArticles()
-      .then(articles => this.articles = articles);
-  }
 }
