080f8d53e495800f06aedbf16bd08ea9638db106
Magnus Grindal Bakken <magnusbakken@gmail.com>
2016-11-18 16:16:38 +0100

    added sorting functionality

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -7,19 +7,73 @@ import 'rxjs/add/operator/toPromise';
 import { Article } from './article';
 import { environment } from '../environments/environment';
 
+/*
+ * [].sort(compare(a, b))
+ * return value
+ *  0 == they are equal in sort
+ *  1 == a comes before b
+ *  -1 == be comes before a
+ */
+interface ArticleSortFn {
+  (a: Article, b: Article): number;
+}
+
+interface ArticleSortOrderFn {
+  (direction: number): ArticleSortFn;
+}
+
+const sortByTime: ArticleSortOrderFn = (direction: number) => (a: Article, b: Article) => {
+  return direction * (b.publishedAt.getTime() - a.publishedAt.getTime());
+};
+
+const sortByVotes: ArticleSortOrderFn = (direction: number) => (a: Article, b: Article) => {
+  return direction * (b.votes - a.votes);
+}; 
+
+const sortFns = {
+  'Time': sortByTime,
+  'Votes': sortByVotes
+};
+
 @Injectable()
 export class ArticleService {
   private _articles: BehaviorSubject<Article[]> =
     new BehaviorSubject<Article[]>([]);
+
+  private _sortByDirectionSubject: BehaviorSubject<number> =
+    new BehaviorSubject<number>(1);
+  private _sortByFilterSubject: BehaviorSubject<ArticleSortOrderFn> =
+    new BehaviorSubject<ArticleSortOrderFn>(sortByTime); 
   
   public articles: Observable<Article[]> = this._articles.asObservable();
 
+  public orderedArticles : Observable<Article[]>;
+
   constructor(
     private http: Http
-  ) { }
+  ) {
+    this.orderedArticles = Observable.combineLatest(
+      this._articles,
+      this._sortByFilterSubject,
+      this._sortByDirectionSubject
+    )
+    .map(([
+      articles, sorter, direction
+    ]) => {
+      return articles.sort(sorter(direction))
+    });
+  }
+
+  public sortBy(
+    filter: string,
+    direction: number
+  ): void {
+    this._sortByDirectionSubject.next(direction);
+    this._sortByFilterSubject.next(sortFns[filter]);
+  }
 
   public getArticles(): void {
-    this._makeHttpRequest('/v1/articles', 'reddit-r-all')
+    this._makeHttpRequest('/v1/articles', 'ars-technica')
       .map(json => json.articles)
       .subscribe(articlesJSON => {
         const articles = articlesJSON
