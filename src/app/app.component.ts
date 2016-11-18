212df04a3219afdad3f389bd9b2da2619eed65e6
Magnus Grindal Bakken <magnusbakken@gmail.com>
2016-11-18 11:59:01 +0100

    first components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-sidebar',
+  template: `
+    <div id="sidebar">
+      Sidebar will go here
+    </div>
+  `
+})
+export class SidebarComponent {}
+
+@Component({
+  selector: 'app-article',
+  template: `
+    <div>Article goes here</div>
+  `
+})
+export class ArticleComponent {}
+
+@Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div id="container">
+      <app-sidebar></app-sidebar>
+      <div id="content">
+        <app-article></app-article>
+      </div>
+    </div>
+  `
 })
 export class AppComponent {
   title = 'app works!';
