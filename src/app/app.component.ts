60ebf6c3de3c9b73499d1a453d349e611c2a6d0d
Magnus Grindal Bakken <magnusbakken@gmail.com>
2016-11-18 13:20:47 +0100

    article service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Article } from './article';
+import { ArticleService } from './article.service'
 
 @Component({
   selector: 'app-root',
@@ -8,25 +9,10 @@ import { Article } from './article';
 export class AppComponent {
   articles: Article[];
 
-  constructor() {
-    this.articles = [
-      new Article(
-        'The Angular 2 screencast',
-        'The easiest way to learn Angular 2 is with Fullstack.io!',
-        10
-      ),
-      new Article(
-        'Fullstack react',
-        'Want to learn React too?',
-      ),
-      new Article(
-        'Vue is new',
-        'And pretty cool syntax too',
-      ),
-      new Article(
-        'But what about elm?',
-        'Everybody likes elm'
-      )
-    ]
+  constructor(
+    private articleService: ArticleService
+  ) {
+    articleService.getArticles()
+      .then(articles => this.articles = articles);
   }
 }
---
+++ src/app/article.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Article } from './article'
+
+@Injectable()
+export class ArticleService {
+
+  constructor() { }
+
+  public getArticles(): Promise<Article[]> {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve([
+          new Article(
+            'The Angular 2 screencast',
+            'The easiest way to learn Angular 2 is with Fullstack.io!',
+            10
+          ),
+          new Article(
+            'Fullstack react',
+            'Want to learn React too?',
+          ),
+          new Article(
+            'Vue is new',
+            'And pretty cool syntax too',
+          ),
+          new Article(
+            'But what about elm?',
+            'Everybody likes elm'
+          )
+        ]);
+      }, 2000);
+    });
+  }
+}
